279de2c91d43fba2b0fae33008c9323742438fa5
bluerain <taewoonlee@gmail.com>
2023-05-06 14:07:31 +0900

    add very good to you

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '2048 Game ver2 very very good!',
+            title: '2048 Game ver2 very very good! to you!',
         })
     ],
     devServer: {
