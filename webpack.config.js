e4d568210b9a1b7b7351532be3db01306a7c94a0
bluerain <taewoonlee@gmail.com>
2023-05-06 15:59:15 +0900

    add ver7 very good!

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '2048 Game ver6',
+            title: '2048 Game ver7 veru good!',
         })
     ],
     devServer: {
